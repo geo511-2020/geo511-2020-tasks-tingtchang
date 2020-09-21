2e9066c8dc1d42db5c17624b9e3e3449976bf9ea
Chang <tingchan@buffalo.edu>
2020-09-21 14:42:03 -0400

    case study three - plot 1

--- week_03/case_study_03.R
+++ week_03/case_study_03.R
@@ -1,1 +1,19 @@
- 
+# Import related library
+library(ggplot2)
+library(gapminder)
+library(dplyr)
+
+# Remove Kuwait from gapminder
+data <- gapminder %>%
+  filter(country != "Kuwait")
+
+p1 <- ggplot(data, aes(lifeExp, gdpPercap, color=continent, size=pop/100000)) + 
+  geom_point() + 
+  facet_wrap(~year, nrow=1) + 
+  scale_y_continuous(trans="sqrt") + 
+  theme_bw() + 
+  labs(x="Life Expectancy", y="GDP per capita",size="Population (100k)", color="continent")
+
+print(p1)
+
+ggsave("plot1.png", width=15, units="in")
